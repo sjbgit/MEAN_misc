412461e99565af8cd745a616f9201474435bd16b
sjbgit <sjb_strat@hotmail.com>
2015-01-19 18:31:19 -0600

    before auth

---
+++ server_auth/server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var jwt = require('jwt-simple')
+var _ = require('lodash')
+var User = require('./user')
+
+var app = express()
+app.use(require('body-parser').json())
+
+var users = [{username: 'dickeyxxx', password: 'pass'}]
+var secretKey  = 'supersecretkey'
+
+function findUserByUsername(username) {
+  return _.find(users, {username: username})
+}
+
+function validateUser(user, password) {
+  return user.password === password
+}
+
+app.post('/session', function (req, res) {
+  var user = findUserByUsername(req.body.username)
+  if (!validateUser(user, req.body.password)) {
+    return res.send(401) // Unauthorized
+  }
+  var token = jwt.encode({username: user.username}, secretKey)
+  res.json(token)
+})
+
+app.get('/user', function (req, res) {
+  var token = req.headers['x-auth']
+  var user = jwt.decode(token, secretKey)
+  // TODO: pull user info from database
+  res.json(user)
+})
+
+
+app.listen(5000)
